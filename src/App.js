cfed2fcc8a93c5e4a4ace69ba1fca113395ca65a
Emma Secrest <emma@Emmas-MBP.attlocal.net>
2021-10-26 16:39:25 -0400

    skeleton created

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+      <div className = 'textBackground'>
+      <h1>Deep Dive</h1>
+      </div>
+      {/* search bar will be created here */}
+      </nav>
+      <div className = 'subreddits'>
+        {/* subreddits will be placed here */}
+        <p>subbreddits placed here</p>
+      </div>
+      <div className = 'feed'>
+        {/*feed will be placed here */}
+        <p> feed will be placed here</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
