5eb121ce39d71f7af2f8a2ad4b30b58750512d38
Emma Secrest <emma@Emmas-MBP.attlocal.net>
2021-10-31 17:43:12 -0400

    attempt 1 to pull information from reddit

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -1,32 +1,25 @@
-import React from "react";
-import { useSelector } from "react-redux";
+// import React from "react";
+import { useSelector , useDispatch} from "react-redux";
+import { useEffect } from "react";
 import { selectFeed } from "./feedSlice";
-import {selectSubreddit, subredditsReducer} from '../subreddits/subredditSlice'
+import { getPosts } from "./feedSlice";
+// import {selectSubreddit, subredditsReducer} from '../subreddits/subredditSlice'
 import "./feed.css"
+import { isLoading } from "./feedSlice";
+// import Subreddits from "../subreddits/Subreddit";
 
 export default function Feed() {
-    const subreddits = useSelector(selectSubreddit);
+    // const subreddits = useSelector(selectSubreddit);
     const feed = useSelector(selectFeed);
+    const dispatch = useDispatch()
 
-   document.getElementsByClassName('sub').onClick = function(){
-       const subredditIdFromFeed = feed.data.subredditId;
-       const subredditIdFromSubreddit = subreddits.data.id;
-
-       if (subredditIdFromFeed === subredditIdFromSubreddit) {
-           return (
-               <ul className = 'posts'>
-                   {Object.values(feed).map(posts =>
-                    <li className = 'feed' key = {posts.data.subredditId[subredditIdFromFeed]}>
-                        <h3>{posts.data.title}</h3>
-                        <p>{posts.data.post}</p>
-                    </li>
-                    
-                    )}
-               </ul>
-           )
-       }
-   }
-     
+useEffect(() => {
+    if(feed){
+        dispatch(getPosts(feed.id))
+    }
+},[feed,dispatch])
+if(isLoading) return <div>Loading</div>
+if(!feed) return null; 
     
     
     return (
--- src/components/feed/feedSlice.js
+++ src/components/feed/feedSlice.js
@@ -1,23 +1,55 @@
-import React from "react";
-import { createSlice } from "@reduxjs/toolkit";
+// import React from "react"
+import { createSlice , createAsyncThunk } from "@reduxjs/toolkit";
+
+
+export const getPosts = createAsyncThunk('posts/getPosts', 
+async (after) => {
+    const res = await fetch(`https://www.reddit.com/hot.json`)
+    const resJson = await res.json();
+    return resJson
+}
+)
+
+
+
 
 const feedSlice = createSlice({
     name: 'feed',
     initialState: {
         feed: [
-            {data: {id:1 , subredditId:1,title: 'title 1' ,post: 'post 1'}},
-            {data: {id:2, subredditId:1, title: "title 2" ,post: 'post 2'}},
-            {data: {id:3, subredditId: 2,title: 'title 3' ,post: "post 3"}},
-            {data: {id:4, subredditId:2,title: 'title 4' ,post:"post 4"}}
+            // {data: {id:1 , subredditId:1,title: 'title 1' ,post: 'post 1'}},
+            // {data: {id:2, subredditId:1, title: "title 2" ,post: 'post 2'}},
+            // {data: {id:3, subredditId: 2,title: 'title 3' ,post: "post 3"}},
+            // {data: {id:4, subredditId:2,title: 'title 4' ,post:"post 4"}}
         ],
     error: false,
     isLoading: false,
     },
-    extraReducers: {}
+    
+    extraReducers: {
+      [getPosts.pending] (state,action){
+          state.isLoading = true;
+          state.error = false;
+      },  
+      [getPosts.rejected] (state,action){
+          state.isLoading = false;
+          state.error = true;
+      },
+      [getPosts.fulfilled](state,action){
+          state.isLoading = false;
+          state.error = false;
+         alert(state.feed.feed)
+        state.feed.feed.push(action.payload)
+        
+      }
+    }
     
     
     
 })
- // fix me 
+ 
  export const feedReducer = feedSlice.reducer
+ export const isLoading = (state) => state.feed.isLoading
+ export const feedError = (state) => state.feed.error
  export const selectFeed = state => state.feed.feed
+
