7a6e8b3bfb4ca96600335dbdf1139068b07e247c
Emma Secrest <esecrest908@gmail.com>
2022-01-21 15:07:04 -0500

    small changes made

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+
 
 ReactDOM.render(
   < Provider store = {store} >
@@ -12,4 +15,7 @@ ReactDOM.render(
   document.getElementById('root')
   );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
